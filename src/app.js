0330018840b296271e73fcfc3b5ca2e9177315b9
RicardoSilvaF <zanphs@gmail.com>
2023-04-09 18:07:12 -0300

    Implantacao do post 'sign-up'

--- src/app.js
+++ src/app.js
@@ -3,6 +3,16 @@ import cors from "cors";
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+
+let users = [];
+let tweets = [];
+
+app.post("/sign-up", (req,res) =>{
+    let newUser = req.body;
+    users.push(newUser);
+    res.send(users);
+});
 
 const port = 5000;
 app.listen(port);
